9f40f89cae10233975deb704f6da755a4debfdc9
Michał Domanowski <mdomanowski11@outlook.com>
2021-06-09 17:24:56 +0000

    Klient z klasami

--- src/com/projekt/Klient.java
+++ src/com/projekt/Klient.java
@@ -7,11 +7,19 @@ public class Klient
 {
     private List<Rezerwacja> rezerwacje;
     private int id = 0; //tymczasowo
+    private String klasa;
 
-    public Klient() { rezerwacje = new LinkedList<>(); }
+    public Klient() {
+        rezerwacje = new LinkedList<>();
+    }
 
-    public void addRezerwacja(Rezerwacja rezerwacja) { rezerwacje.add(rezerwacja); }
-    public List<Rezerwacja> getRezerwacje() { return rezerwacje; }
+    public void addRezerwacja(Rezerwacja rezerwacja) {
+        rezerwacje.add(rezerwacja);
+    }
+
+    public List<Rezerwacja> getRezerwacje() {
+        return rezerwacje;
+    }
 
     public int getId() {
         return id;
@@ -25,5 +33,13 @@ public class Klient
     {
         return "";
     }
-
+    public void setKlasa(int x) {
+        if (x == 1) {
+            klasa = "Pierwsza";
+        } else if (x == 2) {
+            klasa = "Biznesowa";
+        } else if (x == 3) {
+            klasa = "Ekonomiczna";
+        }
+    }
 }
