6d835da2ffd123932b824648f7aa75393947bfdc
Michał Domanowski <mdomanowski11@outlook.com>
2021-06-09 22:31:20 +0000

    Update Bilet.java

--- src/com/projekt/Bilet.java
+++ src/com/projekt/Bilet.java
@@ -3,6 +3,14 @@ package com.projekt;
 public class Bilet {
     private double cena;
     private int nrMiejsca;
+    private int id;
 
-    public Bilet(double cena, int nrMiejsca) { this.cena = cena; this.nrMiejsca = nrMiejsca; }
+    public Bilet(double cena, int nrMiejsca, int id) {
+        this.cena = cena;
+        this.nrMiejsca = nrMiejsca;
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
 }
