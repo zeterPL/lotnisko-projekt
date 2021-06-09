a353645bd08b39b71c091ec6a6ed752e31bbbb2e
Michał Domanowski <mdomanowski11@outlook.com>
2021-06-09 14:35:56 +0000

    Podzial na klasy w miejscach

--- src/com/projekt/TypB.java
+++ src/com/projekt/TypB.java
@@ -7,15 +7,43 @@ public class TypB extends Samolot
     {
         super(id,model);
         this.zasieg = 15900;
-        this.miejsca = 380;
+        this.miejscaPierwszaKl = 80;
+        this.miejscaBiznesowa = 90;
+        this.miejscaEkonomiczna = 210;
         this.type = "TypB";
     }
-    public void zdejmijMiejsce() {
-        miejsca = miejsca--;
+    public void zarezerwujMiejscePierwszaKl() {
+            miejscaPierwszaKl = miejscaPierwszaKl--;
     }
-    public void zwolnijMiejsce() {
-        if (miejsca < 380) {
-            miejsca++;
-        }
+
+    public void zarezerwujMiejsceBiznesowa() {
+            miejscaBiznesowa = miejscaBiznesowa--;
+    }
+
+    public void zarezerwujMiejsceEkonomiczna() {
+            miejscaEkonomiczna = miejscaEkonomiczna--;
+    }
+
+    public void zwolnijMiejscePierwszaKl() {
+            miejscaPierwszaKl++;
+    }
+
+    public void zwolnijMiejsceBiznesowa() {
+            miejscaBiznesowa++;
+    }
+
+    public void zwolnijMiejsceEkonomiczna() {
+            miejscaEkonomiczna++;
+    }
+    public int getMiejscaPierwszaKl() {
+        return miejscaPierwszaKl;
+    }
+
+    public int getMiejscaBiznesowa() {
+        return miejscaBiznesowa;
+    }
+
+    public int getMiejscaEkonomiczna() {
+        return miejscaEkonomiczna;
     }
 }
